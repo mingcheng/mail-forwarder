a89cf528a8d68eb476adb27c83ace04d3c111813
mingcheng <mingcheng@apache.org>
2026-02-15 14:38:06 +0800

    chore: add copyright headers to source files

--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,17 @@
+/*!
+ * Copyright (c) 2026 Ming Lyu, aka mingcheng
+ *
+ * This source code is licensed under the MIT License,
+ * which is located in the LICENSE file in the source tree's root directory.
+ *
+ * File: traits.rs
+ * Author: mingcheng <mingcheng@apache.org>
+ * File Created: 2026-02-11 16:14:32
+ *
+ * Modified By: mingcheng <mingcheng@apache.org>
+ * Last Modified: 2026-02-15 14:28:07
+ */
+
 use async_trait::async_trait;
 
 #[derive(Debug, Clone)]
